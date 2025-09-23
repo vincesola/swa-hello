6b10547685413cc81ed90fd8bda9ba60277e2ff1
vincesola <vincentjsola@gmail.com>
2025-09-23 11:53:45 -0500

    Flatten functions: push-config/register/send-test; add ping

---
+++ api/push-ping/index.js
@@ -0,0 +1,6 @@
+﻿module.exports = async function (context, req) {
+  context.res = {
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ ok: true, ts: new Date().toISOString() })
+  };
+};
